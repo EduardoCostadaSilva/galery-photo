3efdcb1313571913069120859b8fa2a2a130aad6
Eduardocs <eduardocosta5556@gmail.com>
2024-06-26 17:17:57 -0300

    ok

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import { useRef, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/background-image.png');
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
-
+import domtoimage from 'dom-to-image';
 
 
 import ImageViewer from "./components/ImageViewer";
@@ -42,6 +42,7 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
+    if(Platform.OS !== 'web') {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -56,6 +57,23 @@ export default function App() {
       console.log(e);
     }
   }
+  else {
+    try {
+      const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+        quality: 0.95,
+        width: 320,
+        height: 440,
+      });
+
+      let link = document.createElement('a');
+      link.download = 'sticker-smash.jpeg';
+      link.href = dataUrl;
+      link.click();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
